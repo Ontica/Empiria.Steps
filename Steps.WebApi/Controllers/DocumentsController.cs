06670af838e26ce63c0028d3fca0676906240c08
jmcota <jmcota@ontica.org>
2017-10-15 15:45:17 -0400

    Document model and web api (initial version)

---
+++ Steps.WebApi/Controllers/DocumentsController.cs
@@ -0,0 +1,44 @@
+﻿/* Empiria Steps *********************************************************************************************
+*                                                                                                            *
+*  Solution : Empiria Steps                                    System  : Steps Web API                       *
+*  Assembly : Empiria.Steps.WebApi.dll                         Pattern : Web Api Controller                  *
+*  Type     : DocumentsController                              License : Please read LICENSE.txt file        *
+*                                                                                                            *
+*  Summary  : API to get and set documents data.                                                             *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+using System;
+using System.Web.Http;
+
+using Empiria.WebApi;
+using Empiria.WebApi.Models;
+
+using Empiria.Steps.Legal;
+
+namespace Empiria.Steps.WebApi {
+
+  /// <summary>API to get and set documents data.</summary>
+  public class DocumentsController : WebApiController {
+
+    #region GET methods
+
+    [HttpGet]
+    [Route("v1/documents")]
+    public CollectionModel GetDocuments([FromUri] string type = "",
+                                        [FromUri] string keywords = "") {
+      try {
+
+        var documents = Document.GetList(type, keywords);
+
+        return new CollectionModel(this.Request, documents.ToResponse(),
+                                   typeof(Document).FullName);
+      } catch (Exception e) {
+        throw base.CreateHttpException(e);
+      }
+    }
+
+    #endregion GET methods
+
+  }  // class DocumentsController
+
+}  // namespace Empiria.Steps.WebApi
