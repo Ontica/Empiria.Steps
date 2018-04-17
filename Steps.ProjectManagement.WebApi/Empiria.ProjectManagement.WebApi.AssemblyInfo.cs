48d73e15cba6eb81b4348eb73fc258c8f72e308a
jmcota <jmcota@ontica.org>
2018-04-16 23:38:37 -0400

    Add Steps Project Management Web API project

---
+++ Steps.ProjectManagement.WebApi/Empiria.ProjectManagement.WebApi.AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿/* Empiria Governance ****************************************************************************************
+*                                                                                                            *
+*  Solution : Empiria Governance                           System  : Governance Web Api interface            *
+*  Assembly : Empiria.Governance.WebApi.dll                Pattern : Assembly Attributes File                *
+*                                                          License : Please read LICENSE.txt file            *
+*                                                                                                            *
+*  Summary  : Empiria Governance Http/Json web services interface.                                           *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+using System;
+using System.Reflection;
+
+/*************************************************************************************************************
+* Assembly configuration attributes.                                                                         *
+*************************************************************************************************************/
+[assembly: AssemblyTrademark("Empiria and Ontica are either trademarks of La Vía Óntica SC or Ontica LLC.")]
+[assembly: AssemblyCulture("")]
+[assembly: CLSCompliant(true)]
---
+++ Steps.ProjectManagement.WebApi/README.md
@@ -0,0 +1,11 @@
+﻿# Empiria Project Management Web Api License
+
+  Steps Project ManagementSystem web services interface.
+
+# License
+
+  This system is distributed by the GNU AFFERO GENERAL PUBLIC LICENSE.
+
+# Copyright
+
+  Copyright © 2017-2018. La Vía Óntica SC, Ontica LLC and colaborators.
