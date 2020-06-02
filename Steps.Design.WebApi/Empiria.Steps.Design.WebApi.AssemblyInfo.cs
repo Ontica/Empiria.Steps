71d87928a8fb4091b2fecaf7d6db03e80b07efa8
José Manuel Cota <jmcota@ontica.org>
2020-06-02 16:23:20 -0400

    Steps 3.0 Web api module added

---
+++ Steps.Design.WebApi/Empiria.Steps.Design.WebApi.AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿/* Empiria Steps *********************************************************************************************
+*                                                                                                            *
+*  Module   : Steps Design Services                      Component : Web Api                                 *
+*  Assembly : Empiria.Steps.Design.WebApi.dll            Pattern   : Assembly Attributes File                *
+*  Type     : None                                       License   : Please read LICENSE.txt file            *
+*                                                                                                            *
+*  Summary  : Web Api interface used to interact with Empiria Steps Design services.                         *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+using System;
+using System.Reflection;
+
+/*************************************************************************************************************
+* Assembly configuration attributes.                                                                         *
+*************************************************************************************************************/
+[assembly: AssemblyTrademark("Empiria and Ontica are either trademarks of La Vía Óntica SC or Ontica LLC.")]
+[assembly: AssemblyCulture("")]
+[assembly: CLSCompliant(true)]
---
+++ Steps.Design.WebApi/README.md
@@ -0,0 +1,11 @@
+﻿# Empiria Steps Design Web Api
+
+Web Api interface used to interact with Empiria Steps Design services.
+
+# License
+
+This system is distributed by the GNU AFFERO GENERAL PUBLIC LICENSE.
+
+# Copyright
+
+Copyright © 2017-2020. La Vía Óntica SC, Ontica LLC and colaborators.
