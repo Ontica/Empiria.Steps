4850cb9ee797570f31e6a4dba915e4a9fde61da9
José Manuel Cota <jmcota@ontica.org>
2020-06-03 21:10:29 -0400

    GetProcessDesignList() API for public processes added

---
+++ Steps.Design.WebApi/Controllers/ProcessesController.cs
@@ -0,0 +1,40 @@
+﻿/* Empiria Steps *********************************************************************************************
+*                                                                                                            *
+*  Module   : Steps Design Services                        Component : Web Api                               *
+*  Assembly : Empiria.Steps.Design.WebApi.dll              Pattern   : Controller                            *
+*  Type     : ProcessesController                          License   : Please read LICENSE.txt file          *
+*                                                                                                            *
+*  Summary  : Public API used to retrieve processes design data.                                             *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+using System;
+using System.Web.Http;
+
+using Empiria.WebApi;
+
+namespace Empiria.Steps.Design.WebApi {
+
+  /// <summary>Public API used to retrieve processes design data.</summary>
+  public class ProcessesController : WebApiController {
+
+    #region Get methods
+
+    [HttpGet]
+    [Route("v3/steps/design/processes")]
+    public CollectionModel GetProcessDesignList() {
+      try {
+        var list = Process.GetList<Process>("Accessibility = 'Public'");
+
+        return new CollectionModel(this.Request, list.ToResponse(),
+                                   typeof(Process).FullName);
+
+      } catch (Exception e) {
+        throw base.CreateHttpException(e);
+      }
+    }
+
+    #endregion Get methods
+
+  }  // class ProcessesController
+
+}  // namespace Empiria.Steps.Design.WebApi
---
+++ Steps.Design.WebApi/Models/ProcessResponseModels.cs
@@ -0,0 +1,44 @@
+﻿/* Empiria Steps *********************************************************************************************
+*                                                                                                            *
+*  Module   : Steps Design Services                        Component : Web Api                               *
+*  Assembly : Empiria.Steps.Design.WebApi.dll              Pattern   : Response methods                      *
+*  Type     : ProcessResponseModels                        License   : Please read LICENSE.txt file          *
+*                                                                                                            *
+*  Summary  : Response methods for process design data.                                                      *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Empiria.Steps.Design.WebApi {
+
+  /// <summary>Response methods for process design data.</summary>
+  static internal class ProcessResponseModels {
+
+    #region Response methods
+
+    static internal ICollection ToResponse(this IList<Process> list) {
+      ArrayList array = new ArrayList(list.Count);
+
+      foreach (var item in list) {
+        array.Add(item.ToResponse());
+      }
+      return array;
+    }
+
+
+    static internal object ToResponse(this Process process) {
+      return new {
+        uid = process.UID,
+        type= process.StepType.Name,
+        name = process.Name,
+        theme = process.Theme
+      };
+    }
+
+    #endregion Response methods
+
+  }  // class ProcessResponseModels
+
+}  // namespace Empiria.Steps.Design.WebApi
